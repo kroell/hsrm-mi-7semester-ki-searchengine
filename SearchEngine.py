4ba6207644e32c02d4c29e1a96f49b3b0c41b1b0
Soeren Kroell <kroell@idseven.de>
2014-01-14 04:19:24 +0100

    Kommentare und Bugfixing

--- SearchEngine.py
+++ SearchEngine.py
@@ -2,7 +2,23 @@
 # -*- coding: utf-8 -*-
 
 '''
-Search Engine
+A simple PyLucene Search Engine with allows you to index a webpage and do a search on the indexed elements.
+
+
+Copyright © 2014 Soeren Kroell, Marco Wrzalik
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
 '''
 
 import sys
@@ -13,23 +29,23 @@ import nltk
 from bs4 import BeautifulSoup
 import lucene
 
-
-
-URL = 'http://www.spiegel.de/'
+# Global Variables
+URL = 'http://www.spiegel.de'
 SEARCHDEPTH = 1;
 DIR = './index-data'
+
 
 #PAGE
 ##################################
 
 class Page(object):
     '''
-    classdocs
+    Creates a page with html, url, title, description, created and modified
     '''
 
     def __init__(self, html=None, url=None, title=None, description=None, created=None, modified=None, raw=None, tokens=None):
         '''
-        Constructor
+        Inits a page
         '''
         self.html = html
         self.url = url
@@ -44,19 +60,30 @@ class Page(object):
 ##################################
 
 class Spider(object):
+    '''
+    Creates a spider which opens a HTTP URL and analyses this document by getting out the content, title,
+    description. These elemts will be stored in a Page() instance. After standardizing the url, the url 
+    will also be stored in the page instance.
+
+    All founded Pages are hold in a Pages dict, so that every page get´s only one time stored.
+    '''
     
     def __init__(self, start_url):
         '''
-        Constructor
+        Inits a spider with an start URL.
         '''
         self.start_url = start_url
-        self.pages = {} # günstigere Suche wegen Hashing
+        self.pages = {}
     
     
     def initSpider(self, docurl):
-    
+        '''
+        From the start URL, the spider go through all Hyperlinks it founds and analyses this pages also.
+        All calls are recursive. 
+        '''
+        
+        # open an URL
         try:
-            #response = urllib2.urlopen(docurl,timeout=5)
             response = urllib2.urlopen(docurl)
         except urllib2.HTTPError, error:
             print "HTTP error: %r" % error.code
@@ -65,10 +92,11 @@ class Spider(object):
             print "Error: %r" % error
             response = urllib2.urlopen(URL)
 
-    
+        # Get the DOM and close the response
         html = response.read()
         response.close() 
         
+        # Go through the given DOM and get title and so on
         soup = BeautifulSoup(html)
         title = soup.title.string
         description = soup.find("meta", {"property":"og:description"})
@@ -82,55 +110,57 @@ class Spider(object):
         page = Page(html, docurl, title, description, created, modified)
         self.pages[docurl]= page
 
+        # For all founded Hyperlinks, standardize them, so they look all the same
+        # If the spider founds a new url, it calls his self recursivly.
         for link in soup.find_all('a'):
-            #if len(self.pages) >= 20 :
-             #   return
+            if len(self.pages) >= 40 :
+                return
             url = str(link.get('href'))
             try:
                 if url.startswith('/') or url.startswith(self.start_url):
                     if url.startswith('/'):
                         url = self.start_url + url[1:]
                     if not url.endswith('/'):
-                        url = url.rstrip('/') # '/' am ende entfernen
+                        url = url.rstrip('/') # strip '/' at the end
                     if not self.pages.has_key(url):
                         print url
-                        time.sleep(0.2) 
+                        time.sleep(0.2) # take a sleep so the spider never gets blocked by the webpage
                         self.initSpider(url)
             except Exception, error:
                 print "The Spider has done something boring: %r" % error
     
     
     def preparePage(self):
-        #print len(self.pages)
+        '''
+        Cleans the DOM to his raw form.
+        '''
         for page in self.pages.values():
             page.raw = nltk.clean_html(page.html)
-            '''Wird Lucene schon machen'''
-            #page.tokens = nltk.word_tokenize(page.raw)
   
-
-    def getIndexedPages(self):
-        temp = "<ul>"
-        for value in self.pages:
-            print value
-            temp += '<li><a href="'+ value +'" title="' + value + '">' + value + '</a></li>'
-
-        temp += '</ul>'
-        return temp
         
 
 #WRITER
 ##################################
 
 class Writer(object):
+    '''
+    Creates a Writer which can index given documents to definied folder.
+    '''
     
     def __init__(self):
-        lucene.initVM()
-        self.analyzer = lucene.StandardAnalyzer(lucene.Version.LUCENE_CURRENT)
+        '''
+        Inits a Writer by attaching the current luceneVM to the thread and creating a analyzer, a store and a IndexWriter instance.
+        '''
+        vm_env = lucene.getVMEnv() # get lucene.vm
+        vm_env.attachCurrentThread()
+        self.analyzer = lucene.GermanAnalyzer(lucene.Version.LUCENE_CURRENT)
         self.store = lucene.SimpleFSDirectory(lucene.File(DIR))
         self.writer = lucene.IndexWriter(self.store, self.analyzer, True, lucene.IndexWriter.MaxFieldLength(512))
-        print "Currently there are %d documents in the index..." % self.writer.numDocs()
         
     def addPage(self, page):
+        '''
+        Adds a Page to a lucene document and indexing the url, description, content and title.
+        '''
         doc = lucene.Document()
         doc.add(lucene.Field('url', page.url, lucene.Field.Store.YES, lucene.Field.Index.NOT_ANALYZED))
         if (page.description):
@@ -140,12 +170,20 @@ class Writer(object):
         self.writer.addDocument(doc)
 
     def getDoc(self, index):
+        '''
+        Returns a requested Document
+        '''
         return self.store.values()[index]
 
     def close(self):
+        '''
+        Closes the current writer and set´s it null.
+        '''
+        print "Currently are "+ str(self.writer.numDocs()) +" documents indexed. Indexing done!" 
         self.writer.optimize()
         self.writer.close()
         self.writer = None
+
 
     
 
@@ -153,17 +191,28 @@ class Writer(object):
 #READER
 ##################################
 class Reader(object):
+    '''
+    Creates a Read, which provides the possibilty to read an indexed file.
+    '''
 
     def __init__(self):
-        lucene.initVM()
-        self.analyzer = lucene.StandardAnalyzer(lucene.Version.LUCENE_CURRENT)
+        '''
+        Inits a Reader by attaching the current luceneVM to the thread and creating a store and a IndexReader instance.
+        '''
+        vm_env = lucene.getVMEnv() # get lucene.vm
+        vm_env.attachCurrentThread()
         self.store = lucene.SimpleFSDirectory(lucene.File(DIR))
         self.reader = lucene.IndexReader.open(self.store, True)
 
     def getIndexedPages(self):
+        '''
+        Returns a HTML formatted DIV with the number of already indexed files and UL with LI elements, which contains 
+        the item url.
+        '''
         terms = self.reader.terms(lucene.Term("url", ""))
         facets = {'other': 0}
-        temp = "<ul>"
+        temp = "<div><p class='lead'>Derzeit sind "+ str(self.reader.numDocs()) +" Dokumente indexiert.</p></div>" 
+        temp += "<ul>"
         while terms.next():
             if terms.term().field() != "url": 
                 break
@@ -180,10 +229,17 @@ class Reader(object):
 #SEARCHER
 ##################################
 class Searcher(object):
+    '''
+    Creates a Searcher to find a query inside an indexed file.
+    '''
 
     def __init__(self):
-        lucene.initVM()
-        self.analyzer = lucene.StandardAnalyzer(lucene.Version.LUCENE_CURRENT)
+        '''
+        Inits a Searcher by attaching the current luceneVM to the thread and creating a analyzer, a store, a parser and a IndexSearcher instance.
+        '''
+        vm_env = lucene.getVMEnv()
+        vm_env.attachCurrentThread()
+        self.analyzer = lucene.GermanAnalyzer(lucene.Version.LUCENE_CURRENT)
         self.store = lucene.SimpleFSDirectory(lucene.File(DIR))
         self.parser = lucene.MultiFieldQueryParser(
             lucene.Version.LUCENE_CURRENT,
@@ -193,17 +249,19 @@ class Searcher(object):
 
 
     def search(self, queryString):
+        '''
+        Do the actually search for the given queryString and returns an HTML formatted UL with the URL, Title, description and score doc.
+        '''
         query = self.parser.parse(self.parser, queryString)
         scoredocs = self.searcher.search(query, 50).scoreDocs
         MAX = 1000
         hits = self.searcher.search(query, MAX)
-        temp = '<p class="lead">Insgesamt wurden ' + str(hits.totalHits) + ' Dokument(e) für die Suchanfrage "' + str(query) + '" gefunden.</p>'
+        temp = '<p class="lead">Insgesamt wurde(n) ' + str(hits.totalHits) + ' Dokument(e) für die Suchanfrage "' + str(query) + '" gefunden.</p>'
 
         for hit in scoredocs:
-            print hit.score, hit.doc, hit.toString()
+            #print hit.score, hit.doc, hit.toString()
             doc = self.searcher.doc(hit.doc)
-            temp += '<div style="margin-bottom:30px;"><ul class="list-unstyled"><li><h5><a href="' + doc.get("url").encode("utf-8") + '" target="_blank">'+ doc.get("title").encode("utf-8") +'</a></h5> </li><li><a href="'+ doc.get("url").encode("utf-8") +'">'+doc.get("url").encode("utf-8")+' </a></li><li>' + doc.get("description").encode("utf-8") +'</li><li> Score: '+ str(hit.score)+'</li></ul></div>'
- 
+            temp += '<div style="margin-bottom:30px;"><ul class="list-unstyled"><li><h5><a href="' + doc.get("url").encode("utf-8") + '" target="_blank">'+ doc.get("title").encode("utf-8") +'</a></h5> </li><li><a href="'+ doc.get("url").encode("utf-8") +'">'+doc.get("url").encode("utf-8")+' </a></li><li>' + doc.get("description").encode("utf-8") +'</li><li> Score: <b>'+ str(hit.score)+'</b></li></ul></div>'
         return temp
 
 
@@ -212,48 +270,60 @@ class Searcher(object):
 ##################################
 
 class SearchEngine(object):
+    '''
+    Creates a Search Enginge and provides all necessary functions to start the spider, writer or do a search. 
+    These functions are getting used in the WebServer.py 
+    '''
 
     def __init__(self, URL=None):
+        '''
+        Inits a SearchEngine and creating the only lucene VM.
+        '''
+        lucene.initVM() # only initVM once!
         self.url = URL
         self.spider = None
         self.writer = None
         self.searcher = None
 
     def startSpider(self, URL=None):
+        '''
+        Starts the spider on the given URL
+        '''
         spider = Spider(URL)
         spider.initSpider(URL)
         spider.preparePage()
         self.spider = spider
 
     def startWriter (self):
+        '''
+        Starts the Writer and add´s all founded pages to the spider.pages dict.
+        '''
         writer = Writer()    
         [writer.addPage(p) for p in self.spider.pages.values()]
         writer.close()
         self.writer = writer
 
     def startSpiderAndIndex(self, threadName=None, URL=None):
-        print "The Spider is on her way!"
+        '''
+        Is getting called in the WebServer.py to start the spider and indexing.
+        '''
+        print "########## The Spider is on her way! ##########"
         self.startSpider(self.url)
-        print "Now just indexing da things"
+        print "########## Now just indexing da things ##########"
         self.startWriter()
-        print "And we are done!"
-        
 
     def startSearch (self, query):
+        '''
+        Is getting called in the WebServer.py to start a search
+        '''
         searcher = Searcher()
         self.searcher = searcher
         return searcher.search(query)
 
-    def getSpider(self):
-        return self.spider
-
-    def getWriter(self):
-        return self.writer
-
-    def getSearcher(self):
-        return self.searcher
-
     def getIndexedPages(self):
+        '''
+        Is getting called in the WebServer.py to get all indexed Pages
+        '''
         reader = Reader()
         return reader.getIndexedPages()
 
--- WebServer.py
+++ WebServer.py
@@ -1,27 +1,69 @@
-# -*- encoding: UTF-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+A simple CherryPy WebServer which provides you a search form from where you can send your search queries
+to the search engine.
+It show´s you which pages are already indexed an provides a hitlist of your query results with title,
+url, description and hit-score.
+
+
+Copyright © 2014 Soeren Kroell, Marco Wrzalik
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+'''
+
 import os
 
 import thread
 import cherrypy
 import SearchEngine as engine
 
-
+# Global Variables
 URL = 'http://www.spiegel.de/'
-searchEngine = engine.SearchEngine(URL)
+searchEngine = engine.SearchEngine(URL) # create a searchEngine Instance
 indexer = None
 
+
 class StartPage(object):
+    '''
+    The StartPage provides in the index() a search form to send a search query. This form
+    calls the search() function, which sends the start query to the SearchEngine. 
+
+    Both run in a seperate Thread.
+    '''
+
     def index(self):
-        wd = cherrypy.process.plugins.BackgroundTask(20,self.indexThread) # ruft alle 5 Sekunden self.func auf
+        '''
+        Calls the indexThread(), which starts indexing in the background. Also it provides a main html template
+        with a search form.
+        '''
+        wd = cherrypy.process.plugins.BackgroundTask(30,self.indexThread) # start indexing in a seperate Thread
         wd.start()
-        print "Thread-Monster wurde gestartet..."
+        print "Indexing-Thread-Monster has been started..."
         return self.template()
         wd.cancel()
-
-        #wd.cancel()
-    index.exposed = True
+    index.exposed = True # necessary so this page/function can be visited
 
     def search(self, query=None):
+        '''
+        Gets called from the search form and starts a search of the given query. The result will be given
+        inside a html template
+        '''
+        wd = cherrypy.process.plugins.BackgroundTask(1000,self.foo) # start indexing in a seperate Thread
+        wd.start()
+        print "Searching-Thread-Monster has been started..."
         queryResult = searchEngine.startSearch(query)
         return ('''<!doctype html>
 <html lang="en" ng-app="app">
@@ -39,13 +81,13 @@ class StartPage(object):
             <p class="lead">Alles was du schon immer auf Spiegel.de finden wolltest, findest du <a href="/start/">hier</a>.</p>
       </div>
       <div>
-      <!--<form class="form-inline" role="form" method="post" action="search">
+      <form class="form-inline" role="form" method="post" action="search">
               <div class="col-md-10">
                 <input type="text" class="form-control input-lg" id="queryInput" name="query" placeholder="Deine Suchanfrage" required>
               </div>
               
               <button type="submit" class="btn btn-primary btn-lg">Abschicken</button>
-      </form>-->
+      </form>
 
       </div>
       <div id="answer" class="col-md-12"><br/>
@@ -70,10 +112,14 @@ class StartPage(object):
   </script>
 
 </body></html>''') % queryResult
-    search.exposed = True
+        wd.cancel()
+    search.exposed = True # necessary so this page/function can be visited
 
     def template(self):
-    	template = '''<!doctype html>
+        '''
+        Provides a HTML template with a search form. 
+        '''
+        template = '''<!doctype html>
 <html lang="en" ng-app="app">
 <head>
   <meta charset="UTF-8">
@@ -130,19 +176,39 @@ class StartPage(object):
     	return template
 
     def indexThread(self):
+        '''
+        Inits the search engine spider and inits indexing of all found pages
+        '''
         searchEngine.startSpiderAndIndex()
+
+    def foo(self):
+        '''
+        Does nothing, put is necessary :-)
+        '''
+        return # foo is doing just nothing
         
 
 
-
-
 class ShowIndexedPages(object):
+    '''
+    The ShowIndexedPages provides a list of all indexed URL.
+    '''
+
     def index(self):
+        '''
+        Gets all indexed pages from the search engine
+        '''
+        wd = cherrypy.process.plugins.BackgroundTask(1000,self.foo)
+        wd.start()
         indexedPages = searchEngine.getIndexedPages()
         return self.template(indexedPages)
-    index.exposed = True
+        wd.cancel()
+    index.exposed = True # necessary so this page/function can be visited
 
     def template(self, indexedPages):
+        '''
+        Provides a HTML template
+        '''
         return '''<!doctype html>
 <html lang="en" ng-app="app">
 <head>
@@ -175,43 +241,25 @@ class ShowIndexedPages(object):
 </div>
 </body></html>''' % indexedPages
 
+    def foo(self):
+        return # foo is doing just nothing
 
-class IndexStartPage(object):
-    def index(self):
-        wd = cherrypy.process.plugins.BackgroundTask(20,self.indexThread) # ruft alle 5 Sekunden self.func auf
-        wd.start()
-
-        return '''<!doctype html>
-<html lang="en" ng-app="app">
-<head>
-  <meta charset="UTF-8">
-  <title>SearchSpiegel</title>
-  <script src="https://code.jquery.com/jquery.js"></script>
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-<br/>
-<p>Indexierungs-Thread wurde im Hintergrund gestart...</p>
-</div>
-</body>'''
-
-        wd.cancel()
-    index.exposed = True
-
-    def indexThread(self):
-        searchEngine.startSpiderAndIndex()
         
 
 
 class IndexPage(object):
-    start = StartPage()
-    indexed_pages = ShowIndexedPages()
-    index_start = IndexStartPage()
+    '''
+    The IndexPage is the main page, which gets called, if you open your Browser at localhost:8080.
+    It sets the routeNames for all other pages
+    '''
+
+    start = StartPage() # StartPage() ist available under localhost:8080/start/
+    indexed_pages = ShowIndexedPages() # ShowIndexedPages() ist available under localhost:8080/indexed_pages/
 
     def index(self):
-        #bgtask.put(log, "index was called", ip=cherrypy.request.remote.ip))
+        '''
+        Provides a HTML Template, which links to Start and ShowIndexed Page
+        '''
         return '''<!doctype html>
 <html lang="en" ng-app="app">
 <head>
@@ -228,11 +276,11 @@ class IndexPage(object):
 <a href="/index_start/">Indexierungs-Thread aufrufen</a>
 </div>
 </body>'''
-    index.exposed = True
+    index.exposed = True # necessary so this page/function can be visited
 
 
 
-
+# Inits the WebServer with the IndexPage
 cherrypy.quickstart(IndexPage())
 
 
